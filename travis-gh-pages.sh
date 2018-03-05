641d5eadce9b04cbfe0a2cffed9ab24fabb3ac7a
Harald Albrecht <harald.albrecht@siemens.com>
2018-03-05 10:46:57 +0100

    fixes CI

--- travis-gh-pages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone --branch gh-pages --single-branch --depth 1 https://github.com/$TRAVIS_REPO_SLUG gh-pages
-cp $OUTPUT_DIR/thirdflow.html $GH_PAGES_OUTPUT_DIR
-cp $OUTPUT_DIR/thirdflow.tid $GH_PAGES_OUTPUT_DIR
